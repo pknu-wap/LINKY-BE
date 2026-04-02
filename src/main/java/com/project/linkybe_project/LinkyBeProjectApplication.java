92b66baea42fdd25f12dcee7b4a73a5fbe745668
daeyeon0214 <ldykng237@gmail.com>
2026-04-02 20:20:57 +0900

    파일 재생성!!

---
+++ src/main/java/com/project/linkybe_project/LinkyBeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.linkybe_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkyBeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkyBeProjectApplication.class, args);
+    }
+
+}
