49f912b0fb03711fa6e94c10c404dbcfbfc47031
daeyeon0214 <ldykng237@gmail.com>
2026-04-28 22:12:16 +0900

    chore: remove duplicated files

--- src/main/java/com/project/linkybeproject/dto/LinkRequest.java
+++
@@ -1,9 +0,0 @@
-package com.project.linkybeproject.dto;
-
-public class LinkRequest {
-    private String url;
-
-    // Getter & Setter
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
-}
